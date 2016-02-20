699aca63143e03d53799262f6abb17dbc4648f74
leonliang33 <llian008@fiu.edu>
2016-02-20 14:38:56 -0500

    minor

--- app/src/main/java/com/example/leon/uhacks2016/PostConnection.java
+++ app/src/main/java/com/example/leon/uhacks2016/PostConnection.java
@@ -1,47 +1,15 @@
 package com.example.leon.uhacks2016;
 
+import android.bluetooth.BluetoothSocket;
+
 /**
  * Created by Leon on 2/20/2016.
  */
-import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
-import android.view.View;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.app.Activity;
-import android.bluetooth.BluetoothAdapter;
-import android.bluetooth.BluetoothDevice;
 
-import android.content.BroadcastReceiver;
-import android.content.Context;
-import android.content.Intent;
-import android.content.IntentFilter;
-
-import android.graphics.Color;
-import android.net.wifi.ScanResult;
-import android.net.wifi.WifiManager;
-
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-
-import android.widget.ArrayAdapter;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ListView;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Set;
-
-public class PostConnection {
+public class PostConnection extends BluetoothConnect{
+    public BluetoothSocket socket;
     public PostConnection(){
         
     }
+
 }
