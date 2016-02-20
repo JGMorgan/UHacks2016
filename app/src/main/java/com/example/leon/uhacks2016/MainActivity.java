cc3f2c266193b847e2dc075e3d2ba98eaae018bc
Jose <jose@Joses-MacBook-Air.local>
2016-02-20 16:31:33 -0500

    update

--- app/src/main/java/com/example/leon/uhacks2016/MainActivity.java
+++ app/src/main/java/com/example/leon/uhacks2016/MainActivity.java
@@ -45,9 +45,9 @@ import java.util.Set;
 
 public class MainActivity extends AppCompatActivity {
     private BluetoothSocket BlueSocket;
-    public BluetoothAdapter BlueAdapt;
+    public static BluetoothAdapter BlueAdapt;
     public static final int REQUEST_ENABLE_BT = 1;
-    public BroadcastReceiver mReceiver = null;
+    public static BroadcastReceiver mReceiver = null;
     static ArrayList<String> devices;
     NotificationCompat.Builder notification;
     NotificationManager NM;
@@ -68,7 +68,6 @@ public class MainActivity extends AppCompatActivity {
         if(BlueAdapt == null){
             //Bluetooth not supported
         }
-        BluetoothConnect xConnect =new BluetoothConnect();
 
 
         devices = new ArrayList<String>();//need the name of the listview for this
@@ -81,8 +80,8 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-        xConnect.forceEnableBluetooth();
-        xConnect.findDevices();
+        forceEnableBluetooth();
+        findDevices();
         NM.notify(0, notification.build());
 
     }
@@ -110,36 +109,37 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-//    public void forceEnableBluetooth(){
-//        if (!BlueAdapt.isEnabled()) {
-//            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-//            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
-//        }
-//    }
-//
-//    public void findDevices(){
-//        mReceiver = new BroadcastReceiver() {
-//            public void onReceive(Context context, Intent intent) {
-//                String action = intent.getAction();
-//                // When discovery finds a device
-//                if (BluetoothDevice.ACTION_FOUND.equals(action)) {
-//                    // Get the BluetoothDevice object from the Intent
-//                    BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
-//                    // Add the name and address to an array adapter to show in a ListView
-//                    devices.add(device.getName() + "\n" + device.getAddress());
-//                    Log.v("MYSHIT", "YOOOOOOOO");
-//                    Log.v("MYSHIT", devices.toString());
-//                }
-//
-//
-//            }
-//        };
-//
-//        // Register the BroadcastReceiver
-//        IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
-//        registerReceiver(mReceiver, filter); // Don't forget to unregister during onDestroy
-//
-//    }
+    public void forceEnableBluetooth(){
+        if (!BlueAdapt.isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+        }
+    }
+
+    public void findDevices(){
+        IntentFilter f = new IntentFilter(BluetoothAdapter.ACTION_SCAN_MODE_CHANGED);
+        mReceiver = new BroadcastReceiver() {
+            public void onReceive(Context context, Intent intent) {
+                String action = intent.getAction();
+                // When discovery finds a device
+                if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                    // Get the BluetoothDevice object from the Intent
+                    BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                    // Add the name and address to an array adapter to show in a ListView
+                    devices.add(device.getName() + "\n" + device.getAddress());
+                    Log.v("MYSHIT", "YOOOOOOOO");
+                    Log.v("MYSHIT", devices.toString());
+                }
+
+
+            }
+        };
+
+        // Register the BroadcastReceiver
+        IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+        registerReceiver(mReceiver, filter); // Don't forget to unregister during onDestroy
+
+    }
 
     public void PostConnection(){
 
