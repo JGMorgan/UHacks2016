e9322ae0691a27c940ab840f0894d9953cc028f9
Jose <jose@Joses-MacBook-Air.local>
2016-02-20 16:37:34 -0500

    update

--- app/src/main/java/com/example/leon/uhacks2016/MainActivity.java
+++ app/src/main/java/com/example/leon/uhacks2016/MainActivity.java
@@ -146,40 +146,6 @@ public class MainActivity extends AppCompatActivity {
 
 
     }
-//    public void forceEnableBluetooth(){
-//        if (!BlueAdapt.isEnabled()) {
-//            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
-//            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
-//        }
-//    }
-//
-//    public void findDevices(){
-//        mReceiver = new BroadcastReceiver() {
-//            public void onReceive(Context context, Intent intent) {
-//                String action = intent.getAction();
-//                // When discovery finds a device
-//                if (BluetoothDevice.ACTION_FOUND.equals(action)) {
-//                    // Get the BluetoothDevice object from the Intent
-//                    BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
-//                    // Add the name and address to an array adapter to show in a ListView
-//                    devices.add(device.getName() + "\n" + device.getAddress());
-//                    Log.v("MYSHIT", "YOOOOOOOO");
-//                    Log.v("MYSHIT", devices.toString());
-//                }
-//
-//
-//            }
-//        };
-//
-//        // Register the BroadcastReceiver
-//        IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
-//        registerReceiver(mReceiver, filter); // Don't forget to unregister during onDestroy
-//
-//    }
-//
-//    public void PostConnection(){
-//
-//
-//    }
+
 
 }
